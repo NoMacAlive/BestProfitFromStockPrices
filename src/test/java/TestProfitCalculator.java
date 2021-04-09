1013a34979760c79b1e4d63a44bad8915c9f12ff
GuangyaZhu <976670365@qq.com>
2021-04-09 15:18:29 +1200

    finished basic tests, compilation failed

--- src/test/java/TestProfitCalculator.java
+++ src/test/java/TestProfitCalculator.java
@@ -1,4 +1,5 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -11,13 +12,22 @@ import static org.junit.Assert.fail;
  */
 
 public class TestProfitCalculator{
+    private ProfitCalculator calculator;
+    /*
+     * A new calculator for every unit tests
+     */
+    @Before
+    private void initializeCaclulator(){
+        calculator = new ProfitCalculator();
+    }
+
     /**
      * Test that the getMaxProfit function returns the correct max profit with the example test case
      */
     @Test
     public void test_getMaxProfit_with_Example(){
         int[] stockPrices = {10, 7, 5, 8, 11, 9};
-        Assert.assertEquals (6, getMaxProfit(stockPrices)); // returns 6 (buy at $5 sell at $11)
+        Assert.assertEquals (6, calculator.getMaxProfit(stockPrices)); // returns 6 (buy at $5 sell at $11)
     }
 
     /**
@@ -26,7 +36,44 @@ public class TestProfitCalculator{
     @Test
     public void test_getMaxProfit_with_Empty_Input(){
         int[] stockPrices = {};
-        Assert.assertEquals (0, getMaxProfit(stockPrices)); // returns 6 (buy at $5 sell at $11)
+        Assert.assertEquals (0, calculator.getMaxProfit(stockPrices));
+    }
+
+    /**
+     * Test that the getMaxProfit function returns 0 when there is no profit in buying and selling
+     */
+    @Test
+    public void test_getMaxProfit_with_Descending_Input(){
+        int[] stockPrices = {5, 4, 3, 2, 1};
+        Assert.assertEquals (0, calculator.getMaxProfit(stockPrices));
+    }
+
+
+    /**
+     * Test that the getMaxProfit function returns 0 when the stock prices are all the same
+     */
+    @Test
+    public void test_getMaxProfit_with_Same_Number_Input(){
+        int[] stockPrices = {1, 1, 1, 1, 1, 1};
+        Assert.assertEquals (0, calculator.getMaxProfit(stockPrices));
+    }
+
+    /**
+     * Test that the getMaxProfit function returns 0 with all 0 stock prices
+     */
+    @Test
+    public void test_getMaxProfit_with_All_Zero_Input(){
+        int[] stockPrices = {0, 0, 0, 0, 0, 0};
+        Assert.assertEquals (0, calculator.getMaxProfit(stockPrices));
+    }
+
+    /**
+     * Test that the getMaxProfit function returns the correct number with multiple max profit operation
+     */
+    @Test
+    public void test_getMaxProfit_with_Multiple_Max_Profit_Operation(){
+        int[] stockPrices = {10, 7, 5, 3, 11, 9};
+        Assert.assertEquals (6, calculator.getMaxProfit(stockPrices));
     }
 
 
